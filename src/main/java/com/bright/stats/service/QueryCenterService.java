70502e1ab9ca250ccd65223d91200cd9f1e3fd5f
Tz-company <22694172297@qq.com>
2022-09-19 21:08:12 +0800

    init

---
+++ src/main/java/com/bright/stats/service/QueryCenterService.java
@@ -0,0 +1,35 @@
+package com.bright.stats.service;
+
+import com.bright.common.result.PageResult;
+import com.bright.stats.pojo.model.TableHeader;
+import com.bright.stats.pojo.query.QueryCenterQuery;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Author txf
+ * @Date 2022/8/3 16:43
+ * @Description
+ */
+public interface QueryCenterService {
+
+    /**
+     * 获取表头
+     * @param typeCode 模式
+     * @param tableName 表名
+     * @param years 年份
+     * @param months 月份
+     * @return
+     */
+    List<TableHeader> listTableHeaders(String typeCode, String tableName, Integer years, Integer months);
+
+
+    /**
+     * 分页查询
+     * @param queryCenterQuery
+     * @return
+     */
+    PageResult<Map<String, Object>> listTableDataForPage(QueryCenterQuery queryCenterQuery);
+
+}
---
+++ src/main/java/com/bright/stats/service/impl/StatisticsCenterServiceImpl.java
@@ -0,0 +1,38 @@
+package com.bright.stats.service.impl;
+
+import com.bright.common.result.PageResult;
+import com.bright.stats.constant.FileListConstant;
+import com.bright.stats.manager.FileListManager;
+import com.bright.stats.pojo.model.TableHeader;
+import com.bright.stats.pojo.po.primary.FileList;
+import com.bright.stats.pojo.query.StatisticsCenterQuery;
+import com.bright.stats.service.StatisticsCenterService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @Author txf
+ * @Date 2022/8/4 11:18
+ * @Description
+ */
+@Service
+@RequiredArgsConstructor
+public class StatisticsCenterServiceImpl implements StatisticsCenterService {
+
+    private final FileListManager fileListManager;
+
+    @Override
+    public List<TableHeader> listTableHeaders(String typeCode, String tableName, Integer years, Integer months) {
+        FileList fileList = fileListManager.getFileList(typeCode, FileListConstant.FILE_LIST_TABLE_TYPE_BASE, tableName, years, months);
+        return fileList.getTableHeaders();
+    }
+
+    @Override
+    public PageResult<Map<String, Object>> listTableDataForPage(StatisticsCenterQuery statisticsCenterQuery) {
+        return null;
+    }
+}
+
